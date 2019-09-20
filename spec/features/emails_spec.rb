2e1558eac102f72c0e8b236278339fda528a34d8
Javi Martín <javim@elretirao.net>
2019-09-21 01:53:22 +0200

    Simplify investment creation in specs
    Now factories define default headings for investments, so there's no
    need to create a group and a heading to create an investment.
    
    Likewise, in order to create a heading it isn't necessary to specify a
    group anymore; specifying the budget is enough.
    
    It's possible that there are more similar cases we haven't simplified
    yet; I'm only addressing the obvious ones.

--- spec/features/emails_spec.rb
+++ spec/features/emails_spec.rb
@@ -357,8 +357,7 @@ describe "Emails" do
 
     let(:author)   { create(:user, :level_two) }
     let(:budget)   { create(:budget) }
-    let(:group)    { create(:budget_group, name: "Health", budget: budget) }
-    let!(:heading) { create(:budget_heading, name: "More hospitals", group: group) }
+    let!(:heading) { create(:budget_heading, name: "More hospitals", budget: budget) }
 
     scenario "Investment created" do
       login_as(author)
@@ -385,7 +384,7 @@ describe "Emails" do
 
     scenario "Unfeasible investment" do
       budget.update(phase: "valuating")
-      investment = create(:budget_investment, author: author, budget: budget, heading: heading)
+      investment = create(:budget_investment, author: author, budget: budget)
 
       valuator = create(:valuator)
       investment.valuators << valuator
